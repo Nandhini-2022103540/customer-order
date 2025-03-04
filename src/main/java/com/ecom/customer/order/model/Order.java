19864c011cd2eb32aa4f48a8912e679460322045
Nandhini-2022103540 <anandhini3540@gmail.com>
2025-03-04 11:49:34 +0530

    initial commit

---
+++ src/main/java/com/ecom/customer/order/model/Order.java
@@ -0,0 +1,29 @@
+package com.ecom.customer.order.model;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "customer_order")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String productName;
+    private int quantity;
+    private double price;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    @JsonBackReference
+    private com.ecom.customer.order.model.Customer customer;
+}
